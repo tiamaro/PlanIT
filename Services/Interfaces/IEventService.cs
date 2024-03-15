a090eb3eb9bbc54234181dc10cf8efb0d3685eec
tiamaro <tinamariaolsen@gmail.com>
2024-03-15 13:51:24 +0100

    IEventService og EventService

---
+++ Services/Interfaces/IEventService.cs
@@ -0,0 +1,8 @@
+﻿using PlanIT.API.Models.DTOs;
+
+namespace PlanIT.API.Services.Interfaces;
+
+public interface IEventService : IService<EventDTO>
+{
+    Task<EventDTO?> CreateEventAsync(EventDTO newEventDTO);
+}
