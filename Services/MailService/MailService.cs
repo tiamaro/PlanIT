90da87ed86f9af8f87823d53387839ab25551301
HannaPersson95 <Hanna.Persson@gokstadakademiet.no>
2024-03-20 10:13:42 +0100

    Added MailService

--- Services/MailService/MailService.cs
+++ Services/MailService/MailService.cs
@@ -1,6 +1,49 @@
-﻿namespace PlanIT.API.Services.MailService
+﻿using PlanIT.API.Models.Entities;
+using System.Net;
+using System.Net.Mail;
+
+namespace PlanIT.API.Services.MailService;
+
+public class MailService
 {
-    public class MailService
+    public static void SendEmailInvite()
     {
+        try
+        {
+            using (var client = new SmtpClient("smtp-mail.outlook.com", 587))
+            {
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("planit-event@outlook.com", "yVh1to*VzM2c");
+                client.EnableSsl = true;
+
+
+                var message = new MailMessage("planit-event@outlook.com", "hhanna.persson@hotmail.com");
+                message.Subject = "You have been invited to an event!";
+
+                // Insert guest information 
+                //message.Body = $"<h1>Hello, you have been invited to {Event.Name} on {Event.Date} at {Event.Time} at {Event.Location}, best wishes {User.Name}</h1>";
+                message.Body = "<h1> Hello you have been invited to an event </h1>";
+                message.IsBodyHtml = true;
+
+                client.Send(message);
+
+
+            }
+
+
+        }
+
+        catch (Exception ex) 
+        {
+            Console.WriteLine($"Error sending email: {ex.ToString()}");
+
+        }
+        
+
+
     }
+
+
+
 }
