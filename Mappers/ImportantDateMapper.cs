fdd25530b1495305165c52cd086f8d7a377f5f0c
HannaPersson95 <Hanna.Persson@gokstadakademiet.no>
2024-03-22 10:38:15 +0100

    Added important date mapper + Added mailService in program

---
+++ Mappers/ImportantDateMapper.cs
@@ -0,0 +1,25 @@
+﻿using PlanIT.API.Mappers.Interface;
+using PlanIT.API.Models.DTOs;
+using PlanIT.API.Models.Entities;
+
+namespace PlanIT.API.Mappers;
+
+public class ImportantDateMapper : IMapper<ImportantDate, ImportantDateDTO>
+{
+    public ImportantDateDTO MapToDTO(ImportantDate model)
+    {
+        return new ImportantDateDTO(model.Id, model.UserId, model.Name, model.Date);
+    }
+
+    public ImportantDate MapToModel(ImportantDateDTO dto)
+    {
+        return new ImportantDate
+        {
+            Id = dto.Id,
+            UserId = dto.UserId,
+            Name = dto.Name,
+            Date = dto.Date
+            
+        };
+    }
+}
