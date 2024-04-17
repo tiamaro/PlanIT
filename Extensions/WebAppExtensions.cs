b18d11d910c1b569484822bed284704d328206e3
tiamaro <tinamariaolsen@gmail.com>
2024-04-17 16:09:14 +0200

    oppdaterte automatisk registrering av tjenester i DI + fjernet fra time validator i validations

--- Extensions/WebAppExtensions.cs
+++ Extensions/WebAppExtensions.cs
@@ -1,40 +1,44 @@
 ﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace PlanIT.API.Extensions;
 
 public static class WebAppExtensions
 {
 
-    // Metode for å registrere klasser ved hjelp av Dependency Injection
-    public static void RegisterOpenGenericTypeAndDerivatives(this WebApplicationBuilder builder, Type openGenericType)
+    // Metode for å registrere Services i Dependency Injection
+    public static void RegisterServicesFromConfiguration(this IServiceCollection services, Assembly assembly, IConfiguration configuration)
     {
-        var assembly = openGenericType.Assembly;
+        // Last namespaces og exclusions fra konfigurasjon
+        var namespaces = configuration.GetSection("DependencyInjection:Namespaces").Get<string[]>() ?? [];
+        var exclusions = configuration.GetSection("DependencyInjection:Exclusions").Get<string[]>() ?? [];
 
+        // // Behandle hver type og interface basert på de innlastede namespaces og exclusions
         var types = assembly.GetTypes()
-            .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces()
-                .Any(i => IsDerivedFromInterface(i, openGenericType)))
+            .Where(t => t.IsClass && !t.IsAbstract)
             .ToList();
 
         foreach (var type in types)
         {
-            var interfaceType = type.GetInterfaces()
-                .FirstOrDefault(i => IsDerivedFromInterface(i, openGenericType));
-
-            if (interfaceType != null)
+            if (exclusions.Contains(type.Name))
             {
-                builder.Services.AddScoped(interfaceType, type);
+                Console.WriteLine($"Skipped registration of {type.Name}");
+                continue;
+            }
+
+            if (!namespaces.Any(ns => type.Namespace?.Contains(ns) == true))
+                continue;
+
+            foreach (var interfaceType in type.GetInterfaces())
+            {
+                if (interfaceType.Namespace == null || interfaceType.Namespace.StartsWith("System"))
+                    continue;
+
+                services.AddScoped(interfaceType, type);
+                Console.WriteLine($"Registered {interfaceType} to {type}");
             }
         }
     }
-
-
-    // Metode for å sjekke om grensesnittet eller noen av dets overordnede grensesnitt arver fra den angitte generiske typen.
-    private static bool IsDerivedFromInterface(Type interfaceType, Type openGenericType)
-    {
-        return interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == openGenericType ||
-               interfaceType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == openGenericType);
-    }
-
 
 
     // Metode for å hente og validere brukerID fra HttpContext
